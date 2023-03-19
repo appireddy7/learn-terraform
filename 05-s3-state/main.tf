8553d6f3ea8b7174670b37a8d42213df2e3838cc
Appi Reddy <appireddy.777@outlook.com>
2023-03-19 19:52:45 +0530

    terraform state file saving in s3 bucket using ec2 instance

---
+++ 05-s3-state/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-ar"
+    key    = "05-s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-080fb8452a4ae0b04"]
+  tags = {
+    Name = "test"
+  }
+}
