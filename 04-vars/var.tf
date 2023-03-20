e66326e27133b4aec5653259e7f0b30250382f50
Appi Reddy <appireddy.777@outlook.com>
2023-03-20 16:46:03 +0530

    varaible output declaration

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -1,45 +1,66 @@
-variable "sample" {
-  default = "Hello World"
-}
+#variable "sample" {
+#  default = "Hello World"
+#}
+#
+#output "sample" {
+#  value = var.sample
+#}
+#
+#variable "sample1" {}
+#
+#output "sample1" {
+#  value = var.sample1
+#}
+#
+#variable "cli" {}
+#
+#output "cli" {
+#  value = var.cli
+#}
+#
+#variable "sample5" {
+#  default = "Hello"
+#}
+#
+#
+#variable "sample6" {
+#  default = [
+#    "Hello",
+#     1234,
+#    true
+#  ]
+#}
+#
+#
+#variable "sample7" {
+#  default = {
+#    string = "Hello",
+#    number =  1234,
+#    boolean = true
+#  }
+#}
+#
+#output "types" {
+#  value = "Variable sample5 = ${var.sample5}, First value in the list = ${var.sample6[0]}, Boolean value of map = ${var.sample7["boolean"]} "
+#}
 
-output "sample" {
-  value = var.sample
-}
-
-variable "sample1" {}
-
-output "sample1" {
-  value = var.sample1
-}
-
-variable "cli" {}
-
-output "cli" {
-  value = var.cli
-}
-
-variable "sample5" {
-  default = "Hello"
-}
-
-
-variable "sample6" {
+variable "d1" {
   default = [
-    "Hello",
-     1234,
-    true
+    {
+      course_name   = "Devops"
+      trainer_name  = "Raghu"
+    },
+    {
+      course_name   = "Aws"
+      trainer_name  = "Ravi"
+    }
   ]
 }
 
-
-variable "sample7" {
-  default = {
-    string = "Hello",
-    number =  1234,
-    boolean = true
-  }
+output "course_names" {
+  value = var.d1.*.course_name
 }
 
-output "types" {
-  value = "Variable sample5 = ${var.sample5}, First value in the list = ${var.sample6[0]}, Boolean value of map = ${var.sample7["boolean"]} "
+output "trainer_names" {
+  value = var.d1.*.trainer_name
 }
