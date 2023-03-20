1e57b70b78e102777f7ee89f5af0a1f2226a8ce4
Appi Reddy <appireddy.777@outlook.com>
2023-03-20 21:38:21 +0530

    loops basics

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -5,3 +5,11 @@ variable "d1" {
 resource "null_resource" "nothing" {
   count = var.d1
 }
+
+variable "d2" {
+  default = ["apple","grape"]
+}
+
+resource "null_resource" "d2" {
+  count = length(var.d2)
+}
