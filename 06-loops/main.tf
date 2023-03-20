144d0b213e309c879a0c588d97148bdb200d2e19
Appi Reddy <appireddy.777@outlook.com>
2023-03-20 21:44:24 +0530

    loops map of maps

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -13,3 +13,21 @@ variable "d2" {
 resource "null_resource" "d2" {
   count = length(var.d2)
 }
+
+variable "d3" {
+  default = {
+    apple = {
+      name = "apple"
+      count = 3
+    }
+
+    grape = {
+      name = "grape"
+      count = 30
+    }
+  }
+}
+
+resource "null_resource" "d3" {
+  for_each = var.d3
+}
