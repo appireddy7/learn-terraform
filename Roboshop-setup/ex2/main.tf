e4a8b839c20730db4b60c121ee9cbb4daf0f21eb
Appi Reddy <appireddy.777@outlook.com>
2023-03-20 17:22:50 +0530

    creating multiple servers using length function and loops concept and priniting public ips of created servers

--- Roboshop-setup/ex2/main.tf
+++ Roboshop-setup/ex2/main.tf
@@ -5,15 +5,22 @@ data "aws_ami" "ami" {
 }
 
 resource "aws_instance" "frontend" {
-  count = 5
-  ami = "ami-0089b8e98cd95257d"
-  instance_type = "t3.micro"
+  count                  = length(var.instances)
+  ami                    = "ami-0089b8e98cd95257d"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-080fb8452a4ae0b04"]
+  tags                  = {
+    Name                = var.instances[count.index]
+  }
 }
 
-resource "aws_instance" "user" {
-  count = 2
-  ami = "ami-0089b8e98cd95257d"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-080fb8452a4ae0b04"]
+variable "instances" {
+  default = ["cart","catalogue","user","payment","shipping","rabbitmq"]
 }
+
+output "public_ip" {
+  value = aws_instance.frontend.*.public_ip
+}
+
+
+
