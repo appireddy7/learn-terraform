c7ec52c8c56094ccadc0f3113555ce0eed3167ba
Appi Reddy <appireddy.777@outlook.com>
2023-03-21 21:45:50 +0530

    conditions demonstration with tobool

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -3,3 +3,10 @@ resource "aws_instance" "test" {
   instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
 }
 variable "instance_type" {}
+
+variable "create_instance" {}
+resource "aws_instance" "test1" {
+  count = tobool(var.create_instance) ? 1 :0
+  ami = "ami-0089b8e98cd95257d"
+  instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
+}
