a52cd8a6cb3df2111ca3285c1969808216035920
Appi Reddy <appireddy.777@outlook.com>
2023-04-03 20:48:56 +0530

    provisioner

---
+++ 10-provisioners/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0089b8e98cd95257d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-080fb8452a4ae0b04"]
+  tags                   = {
+    Name = "dev"
+  }
+
+
+  provisioner "remote-exec" {
+    connection {
+      user     = "root"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+  }
+}
+
+inline = [
+"echo hello"
+]
