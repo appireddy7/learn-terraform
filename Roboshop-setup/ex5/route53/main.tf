94388197c2e18487d03fc51ef26c067557002a2e
Appi Reddy <appireddy.777@outlook.com>
2023-04-03 11:35:18 +0530

    share the data from one module to other from other module to another module

---
+++ Roboshop-setup/ex5/route53/main.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "record" {
+  zone_id = "Z094015313BESRDU9JXJH"
+  name    = "${var.component}-dev.devopsar.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
